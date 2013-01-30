072c26ea7af9362771b012b9a82cfe858d7ac350
Dennis Walvoort <dennis.Walvoort@wur.nl>
2013-01-30 00:00:00 +0000

    version 0.3-3

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,4 @@
 .onLoad <-
 function(libname, pkgname) {
     .jpackage(pkgname)
-    javaVersion <- utils:::packageDescription(pkg = pkgname, fields = "SystemRequirements") 
-    packageStartupMessage("Note: ", sQuote(pkgname), " requires ",
-        javaVersion, ", which is available at www.java.com")
 }  
